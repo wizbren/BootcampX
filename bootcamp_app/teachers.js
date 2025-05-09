e9e492b93e9c9f85ced6c05a5225d544780f329b
Brendan Brown <brendan94brown@gmail.com>
2025-05-09 12:40:36 -0700

    Change structure of the file for organization, using more variables, and adding parameterized queries to protect agianst SQL injection attacks

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-  ORDER BY teacher;
-  `)
+
+const cohortName = process.argv[2] || 'JUL02';
+const query = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
+
+pool.query(query, [cohortName])
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
-    })
-  });
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
